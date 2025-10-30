6c15b0b3c533a3005b9ba90e01b371ebeb8e68ee
nickiiinicole <nicki.nicoledc04@gmail.com>
2025-10-30 17:23:04 +0100

    Ejercicios terminados:D

--- Ejercicio32/main.py
+++ Ejercicio32/main.py
@@ -1,10 +1,21 @@
 def ask_marks(subjects):
-    
-    mark = float(input("Introduce tu nota: "))
-    
+
+    marks = []
+    for subject in subjects:
+        input_valid = False
+        while not input_valid:
+            mark =float(input(f"Introduzca la nota de la asignatura {subject}: "))
+            if mark<0 or mark>10:
+                print("Porfavor introduzca una nota valida")
+            else:
+                input_valid=True
+        marks.append(mark)     
+    return marks
 
 subjects = ["Matemáticas", "Física", "Química", "Historia", "Lingua"]
+subjects.sort(key=str.lower)
+marks = ask_marks(subjects)
+for i in range(0, len(subjects)):
+    print(f"En la materia {subjects[i]} sacaste un {marks[i]:.2f}")
 
-notas = []
-print("Eu estudo: " + ", ".join(subjects))
 
