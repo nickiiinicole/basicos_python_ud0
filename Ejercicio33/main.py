6b0463e9117152af4f7759ee6657c44aed8ab681
nickiiinicole <nicki.nicoledc04@gmail.com>
2025-11-06 09:22:33 +0100

    ejercicio 33 hechito

--- Ejercicio33/main.py
+++ Ejercicio33/main.py
@@ -1,12 +1,16 @@
-import math
+def loteria():
+    
+    list_win_numbers = []
+    contador= 0
+    number = False
+    while len(list_win_numbers)<6:
+        win_number= int(input("Introduce el numero ganandor de loteria: "))
+        if  1 <= win_number <= 49:#1-49 los nunmeros de loteria
+            list_win_numbers.append(win_number)
+        else:
+            print("Numero invalido, introduzca un numero entre el rango 1 y 49.")
+    # sorted -> devuelve otra lista 
+    list_win_numbers.sort(reverse=True)
+    return list_win_numbers
 
-def area_circle(radio):
-
-    return math.pi*radio**2
-
-def volume_cylinder(radio, height):
-    return area_circle(radio) * height
-
-radio = float(input("Introduce el radio: "))
-height = float(input("Introduce la altura: "))
-print(f"El valor del area del circulo es {area_circle(radio):.2f} y el volumen del cilindro: {volume_cylinder(radio,height):.2f}")
+print(f"El numero de loteria es {loteria()}")
