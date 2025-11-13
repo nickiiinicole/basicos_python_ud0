0bc9c18d393e1b5b7a4d0794b0c70461f0f276bc
nickiiinicole <nicki.nicoledc04@gmail.com>
2025-11-13 10:40:24 +0100

    ejrcicio42

---
+++ Ejercicio42/main.py
@@ -0,0 +1,16 @@
+import math
+def area_circle(radio):
+    if radio<=0:
+        return None
+    
+    return format(math.pi * radio**2, "2.f")
+
+def volume_cilinder(radio, height):
+
+    if height<=0 or radio<=0:
+        return None
+    
+    return format(area_circle(radio)*height,".2f")
+
+
+
---
+++ Ejercicio42/tests_ejercicio42.py
@@ -0,0 +1,19 @@
+from main import area_circle,volume_cilinder
+
+def assert_area_circle_positive():
+    assert area_circle(1)=="3.14"
+    assert area_circle(5)=="78.54"
+    assert area_circle(10)== "314.16"
+
+def assert_limit_numbers():
+    assert area_circle(0)==None
+    assert area_circle(-1)==None
+    assert volume_cilinder(0,10)==None
+    assert volume_cilinder(-1.5,3)==None
+    assert volume_cilinder(-1.5,120)==None
+
+def assert_volume_cilinder_positive():
+    assert volume_cilinder(4,10)=="502.65"
+    assert volume_cilinder(0.5,2)=="1.57"
+    assert volume_cilinder(1.5,3)== "21.21"
+    
