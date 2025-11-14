e896c996300c4bd24d906ef78880545f7b021f4b
nickiiinicole <nicki.nicoledc04@gmail.com>
2025-11-14 09:11:45 +0100

    q

--- src/basicos_python/Ejercicio42/main.py
+++ src/basicos_python/Ejercicio42/main.py
@@ -3,11 +3,14 @@ import math
 def area_circle(radio):
     if radio <= 0:
         return None
-    return format(math.pi * radio**2,".2f")
+    
+    return math.pi * radio**2 
 
 def volume_cilinder(radio, height):
     
     if height <= 0 or radio<=0:
         return None
-
-    return format(area_circle(radio) * height,".2f")
+    
+    area_base = area_circle(radio)
+    
+    return format(area_base * height, ".2f")
